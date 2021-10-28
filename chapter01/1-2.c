6fd586fd5691ad9826334f89a40b6167ab76250c
abe-101 <slimhacker@pm.me>
2021-10-28 09:21:19 -0400

    prints's argument string contains \c

---
+++ chapter01/1-2.c
@@ -0,0 +1,14 @@
+/* 
+ * Chapter 1 Exercise 01.c Run the "hello, world" program on your system.
+ * Experiment with leaving parts of the program, to see what
+ * error messages you get.
+ *
+ * By @abe_SlimHacker
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\c");
+}
